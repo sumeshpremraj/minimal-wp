e85c45e33ec378178bd3f920f123d84d4726d8f7
Sumesh <sumeshpremraj@gmail.com>
2013-10-26 17:48:03 +0530

    Fixed footer links

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
     <div id="footer">
         <div class="centerCol">
             <div class="copyright">
-                <a href="http://wordpress.org">WordPress</a> + <a href="">MinimalWP</a> + <a href=" https://www.digitalocean.com/?refcode=7f10667e7318
+                <a href="http://wordpress.org">WordPress</a> + <a href="https://github.com/sumeshpremraj/minimal-wp">MinimalWP</a> + <a href=" https://www.digitalocean.com/?refcode=7f10667e7318
 ">DigitalOcean</a>
                 <br/>
-                &copy; 2013 Sumesh P.
+                &copy; 2013 <?php bloginfo('name'); ?>.
             </div>
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
+            <a href="<?php bloginfo('home'); ?>">Home</a>
+            <a href="<?php bloginfo('home'); ?>/about/">About</a>
+            <a href="<?php bloginfo('home'); ?>/contact/">Contact</a>
             
             <!--
             <a href="">Photos</a>
